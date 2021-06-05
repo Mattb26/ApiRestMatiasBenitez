3f665cad3a49a0e556b52791a865c8753e63e839
Mattb26 <81666147+Mattb26@users.noreply.github.com>
2021-06-04 22:47:37 -0300

    Add files via upload

---
+++ Api Rest Matias Benitez/Api Rest Matias Benitez/Controllers/Apis.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_Rest_Matias_Benitez.Moledo;
+using Api_Rest_Matias_Benitez.Repositorios;
+
+namespace Api_Rest_Matias_Benitez
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Apis : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPAlumnos rpAlum = new RPAlumnos();
+            return Ok(rpAlum.ObtenerAlumnos());
+        }
+
+        [HttpGet ("{DNI}")]
+        public IActionResult Get(int DNI)
+        {
+            RPAlumnos rpAlum = new RPAlumnos();
+
+            var AlumRet = rpAlum.ObtenerAlumnos(DNI);
+            if (AlumRet == null)
+            {
+                var nf = NotFound("El cliente " + DNI.ToString() + " no existe.");
+                return nf;
+            }
+            return Ok(AlumRet);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarCliente(Alumnos nuevoAlumno)
+        {
+            RPAlumnos rpCli = new RPAlumnos();
+            rpCli.Agregar(nuevoAlumno);
+            return CreatedAtAction(nameof(AgregarCliente), nuevoAlumno);
+        }
+    }
+}
---
+++ Api Rest Matias Benitez/Api Rest Matias Benitez/Moledo/Alumnos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Rest_Matias_Benitez.Moledo
+{
+    public class Alumnos
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Curso { get; set; }
+        public int Año { get; set; }
+    }
+}
